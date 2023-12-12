ec1ac8aef82105f03535247603182a33f6d78e52
adomasr <adomas.razmus@gmail.com>
2023-12-12 23:02:50 +0000

    Homepage Display Updates

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -14,7 +14,10 @@ import { RouterModule } from '@angular/router';
     <section class="listing">
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}, {{housingLocation.country}}</p>
+      <p class="units-available">Units available: <span>{{
+        housingLocation.availableUnits > 5? '+5': housingLocation.availableUnits
+      }}</span></p>
       <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
@@ -22,7 +25,11 @@ import { RouterModule } from '@angular/router';
 })
 
 export class HousingLocationComponent {
-
   @Input() housingLocation!: HousingLocation;
-
 }
+
+
+// Notes:
+//   * had to assign a class for <p> displaying Units available and style it in the CSS file.
+//   * placed {{housingLocation.availableUnits}} into <span> and styled it to guide users attention to the important info
+
